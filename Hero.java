6f189dea3e31ce5009f8f0096c6b7c86e6c60a8f
Jackyjackyjackyjackyjackyjackyjacky <349922815@gapps.yrdsb.ca>
2024-09-12 13:02:16 -0400

    added hero and object to dodge

---
+++ Hero.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Hero extends Actor
+{
+    boolean moved = false;
+    public void act()
+    {
+        if(Greenfoot.mouseClicked(null))
+        {
+            moved = !moved;
+        }
+        
+        if(moved)
+        {
+            setLocation(100,300);
+        }
+        else
+        {
+            setLocation(100, 100);
+        }
+    }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -16,6 +16,10 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 400, 1);
+        Hero hero = new Hero();
+        addObject(hero, 100, 100);
+        Lift lift = new Lift();
+        addObject(lift, 600, 100);
     }
 }
